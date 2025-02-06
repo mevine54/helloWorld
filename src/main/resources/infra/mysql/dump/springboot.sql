303915c39ac112c9a91f965b4add5c555dcf6a6b
mevine54 <mevinecascadeur@gmail.com>
2025-02-06 14:14:04 +0100

    tests ok hellowew ok2

--- src/main/resources/infra/mysql/dump/springboot.sql
+++ src/main/resources/infra/mysql/dump/springboot.sql
@@ -1,39 +1,39 @@
 CREATE DATABASE springboot;
-use springboot;
+USE springboot;
 
 DROP TABLE IF EXISTS `Person`;
 
 CREATE TABLE `Person` (
-  `id` int unsigned NOT NULL auto_increment,
-  `lastname` varchar(50) default NULL,
-  `firstname` varchar(50) default NULL,
-  PRIMARY KEY (`id`)
+                          `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                          `lastname` VARCHAR(50) DEFAULT NULL,
+                          `firstname` VARCHAR(50) DEFAULT NULL,
+                          PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=1;
 
-INSERT INTO `Person` (`lastname`,`firstname`)
+INSERT INTO `Person` (`lastname`, `firstname`)
 VALUES
-  ("Hyde","Jemima"),
-  ("Thornton","Adele"),
-  ("Ramirez","Shellie"),
-  ("Pennington","Kelly"),
-  ("Forbes","Bruno"),
-  ("Britt","Quinn"),
-  ("Glass","Jescie"),
-  ("Booker","Thane"),
-  ("Riddle","Lance"),
-  ("Kline","Rylee"),
-  ("Neal","Sonia"),
-  ("Mitchell","Neil"),
-  ("Saunders","Alexis"),
-  ("Watkins","Branden"),
-  ("Vaughan","Emi"),
-    ("Hancock","Palmer"),
-  ("Holloway","Isabella"),
-  ("Cotton","Silas"),
-  ("Wong","Ryder"),
-  ("Cooper","Nero"),
-    ("Delacruz","Tallulah"),
-  ("Sullivan","Imelda"),
-  ("Stewart","Orlando"),
-  ("Drake","Thor"),
-  ("Strickland","Adrienne");
+    ('Hyde', 'Jemima'),
+    ('Thornton', 'Adele'),
+    ('Ramirez', 'Shellie'),
+    ('Pennington', 'Kelly'),
+    ('Forbes', 'Bruno'),
+    ('Britt', 'Quinn'),
+    ('Glass', 'Jescie'),
+    ('Booker', 'Thane'),
+    ('Riddle', 'Lance'),
+    ('Kline', 'Rylee'),
+    ('Neal', 'Sonia'),
+    ('Mitchell', 'Neil'),
+    ('Saunders', 'Alexis'),
+    ('Watkins', 'Branden'),
+    ('Vaughan', 'Emi'),
+    ('Hancock', 'Palmer'),
+    ('Holloway', 'Isabella'),
+    ('Cotton', 'Silas'),
+    ('Wong', 'Ryder'),
+    ('Cooper', 'Nero'),
+    ('Delacruz', 'Tallulah'),
+    ('Sullivan', 'Imelda'),
+    ('Stewart', 'Orlando'),
+    ('Drake', 'Thor'),
+    ('Strickland', 'Adrienne');
