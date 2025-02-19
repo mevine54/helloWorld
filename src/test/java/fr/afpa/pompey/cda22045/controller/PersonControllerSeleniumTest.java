c1b284079c350d5a53b59b4df721bea37c920a18
mevine54 <mevinecascadeur@gmail.com>
2025-02-19 13:34:40 +0100

    github action création image api

--- src/test/java/fr/afpa/pompey/cda22045/controller/PersonControllerSeleniumTest.java
+++ src/test/java/fr/afpa/pompey/cda22045/controller/PersonControllerSeleniumTest.java
@@ -14,35 +14,35 @@ class PersonControllerSeleniumTest {
 
     private WebDriver driver;
 
-    @BeforeEach
-    public void setUp() {
-        // emplacement du pilote Chrome
-//        System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
-
-        // Instanciation du pilote Chrome
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @Test
-    public void testPageTitle() {
-        // Accédez à une URL
-        driver.get("http://localhost:9000");
-
-        // Récupérez le titre d la page
-        String title = driver.getTitle();
-
-        // Vérifiez si le titre est correct
-        assertEquals("HelloWorld", title);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        // Fermez le navigateur
-        driver.quit();
-    }
+//    @BeforeEach
+//    public void setUp() {
+//        // emplacement du pilote Chrome
+////        System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+//
+//        // Instanciation du pilote Chrome
+//        WebDriverManager.chromedriver().setup();
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--headless");
+//        driver = new ChromeDriver(options);
+//    }
+//
+//    @Test
+//    public void testPageTitle() {
+//        // Accédez à une URL
+//        driver.get("http://localhost:9000");
+//
+//        // Récupérez le titre d la page
+//        String title = driver.getTitle();
+//
+//        // Vérifiez si le titre est correct
+//        assertEquals("HelloWorld", title);
+//    }
+//
+//    @AfterEach
+//    public void tearDown() {
+//        // Fermez le navigateur
+//        driver.quit();
+//    }
 }
